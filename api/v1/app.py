39181283ba944ea9325da3a7f9addc8368e90e2d
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-07-08 09:27:09 +0200

    Include organistation route in app module

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 from routers import userRoutes
+from routers import organisationRoutes
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 """
 App module entry point to server
@@ -11,7 +12,7 @@ router = APIRouter()
 
 
 app.include_router(userRoutes.router)
-
+app.include_router(organisationRoutes.router)
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return RedirectResponse(url="/docs")
