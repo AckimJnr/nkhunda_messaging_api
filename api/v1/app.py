5cb4a2707083561c276eb95922494dccdd911519
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-07-10 16:29:24 +0200

    Refactor imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,18 @@
-from fastapi import FastAPI, APIRouter, Depends, HTTPException
+from fastapi import FastAPI, APIRouter
 from fastapi.responses import RedirectResponse
-from routers import userRoutes
-from routers import organisationRoutes
-from routers import organisationApplicationRoutes
-from routers import messageRoutes
-from routers import authRoutes
-"""
-App module entry point to server
-"""
+from routers import userRoutes, organisationRoutes, organisationApplicationRoutes, messageRoutes, authRoutes
 
 app = FastAPI()
 router = APIRouter()
 
-app.include_router(authRoutes.router)
 app.include_router(userRoutes.router)
 app.include_router(organisationRoutes.router)
 app.include_router(organisationApplicationRoutes.router)
 app.include_router(messageRoutes.router)
+app.include_router(authRoutes.router)
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return RedirectResponse(url="/docs")
-
 
 app.include_router(router)
